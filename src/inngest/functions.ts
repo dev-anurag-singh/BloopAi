3ea8d6c132bd67dbffb101b77a1ef4b4ad655bb4
Anurag Kumar <dev.anuragkr@gmail.com>
2025-06-27 22:13:05 +0530

    test: inngest functions

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,22 @@
 import { inngest } from "./client";
+import { openai, createAgent } from "@inngest/agent-kit";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+export const writeContents = inngest.createFunction(
+  { id: "write-contents" },
+  { event: "app/ticket.created" },
+  async ({ event }) => {
+    // Create a new agent with a system prompt (you can add optional tools, too)
+    const writer = createAgent({
+      name: "writer",
+      system:
+        "You are an expert writer.  You write readable, concise, simple content.",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    // Run the agent with an input.  This automatically uses steps
+    // to call your AI model.
+    const { output } = await writer.run(`Write a tweet on ${event.data.value}`);
+
+    return { output };
   }
 );
